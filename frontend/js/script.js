b51f520ec437c4fa4494879aefee3425a2c6d5e5
Enzo <enzowarikoda@gmail.com>
2026-06-09 16:39:32 -0300

    Adicionado barra de pesquisa e dialog de confirmacao para exclusao

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -5,9 +5,15 @@ const dialog = document.querySelector('#dialogTodo');
 const openDialogBtn = document.querySelector('#abrirDialog');
 const closeDialogBtn = document.querySelector('#fecharDialog');
 const dialogTitle = document.querySelector('#dialogTitle');
+const searchTodo = document.querySelector('.search-todo');
+const dialogDelete =document.querySelector('#dialogDelete');
+const cancelDelete =document.querySelector('#cancelDelete');
+const confirmDelete = document.querySelector('#confirmDelete');
 
 let editingTodoId = null;
 let editingTodoStatus = false;
+let todosCache = []
+let todoToDelete = null;
 
 const fetchTodos = async () => {
     const response = await fetch(
@@ -49,7 +55,10 @@ const createRow = (todo) => {
     checkbox.addEventListener('change',({ target }) => { toggleStatus( id, title, target.checked ); });
 
     editBtn.addEventListener('click',() => { editTodo(id, title, status); });
-    deleteBtn.addEventListener('click', () => { deleteTodo(id); });
+    deleteBtn.addEventListener('click', () => { 
+        todoToDelete = id;
+        dialogDelete.showModal();
+    });
 
     tdActions.append( editBtn, deleteBtn);
 
@@ -59,12 +68,18 @@ const createRow = (todo) => {
 
 };
 
+const renderTodos = (todos) => {
+    tbody.innerHTML = '';
+
+    todos.forEach(todo => {
+        createRow(todo);
+    });
+}
+
 const loadTodos = async () => {
-    const todos = await fetchTodos();
+    todosCache = await fetchTodos();
 
-    tbody.innerHTML='';
-
-    todos.forEach(todo => createRow(todo));
+    renderTodos(todosCache);
 };
 
 const createTodo = async (event) => {
@@ -151,6 +166,36 @@ openDialogBtn.addEventListener('click', () => {
 });
 
 closeDialogBtn.addEventListener('click', () => { dialog.close(); });
+
+searchTodo.addEventListener('input', (event) => {
+    const value = event.target.value.toLowerCase();
+
+    const filteredTodos = todosCache.filter(todo =>
+        todo.title.toLowerCase().includes(value)
+    );
+
+    renderTodos(filteredTodos);
+});
+
+cancelDelete.addEventListener('click', () => {
+    todoToDelete = null;
+
+    dialogDelete.close();
+});
+
+confirmDelete.addEventListener('click', async () => {
+    if (!todoToDelete) {
+        return;
+    }
+
+    await deleteTodo(
+        todoToDelete
+    );
+
+    todoToDelete = null;
+
+    dialogDelete.close();
+});
 
 formTodo.addEventListener('submit', createTodo);
 
